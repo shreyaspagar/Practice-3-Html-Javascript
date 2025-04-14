7500c716f4e7a9de0c97aefcb9ee91a6d11493f8
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-14 19:08:39 +0530

    Add files via upload

---
+++ app12.js
@@ -0,0 +1,17 @@
+let str = "ample";
+if (str[0] === 'a' && str.length > 3) {
+    console.log("good string");
+}
+else {
+    console.log("not a good string");
+}
+
+let num = 12;
+
+if (num % 3 === 0 && (num + 1 === 15 || num - 1 === 11 )) {
+    console.log("safe");
+
+} 
+else {
+    console.log("unsafe");
+}
